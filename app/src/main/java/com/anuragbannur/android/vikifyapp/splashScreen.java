a7ffa7b34a8616debdd1f001f7372fd033af8046
AnuragBannur <bannuranurag@gmail.com>
2018-08-11 12:31:30 +0530

    Initial commit 1

--- app/src/main/java/com/anuragbannur/android/vikifyapp/splashScreen.java
+++ app/src/main/java/com/anuragbannur/android/vikifyapp/splashScreen.java
@@ -6,13 +6,16 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class splashScreen extends AppCompatActivity {
+
     private static  int SPLASH_TIME_OUT=1750;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override

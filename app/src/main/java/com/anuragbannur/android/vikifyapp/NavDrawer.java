dcdc2095a2183014d14b98c5e8013b3bd9a33791
AnuragBannur <bannuranurag@gmail.com>
2018-08-22 09:30:06 +0530

    Working on the Nav drawer. Back to constraint layout. New NavDrawer Activity

---
+++ app/src/main/java/com/anuragbannur/android/vikifyapp/NavDrawer.java
@@ -0,0 +1,13 @@
+package com.anuragbannur.android.vikifyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavDrawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_drawer);
+    }
+}
